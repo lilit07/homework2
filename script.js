222daa4f2da51fb01489234e25644c2303f175e7
Lilit <khachatryanlilit07@gmail.com>
2018-09-13 12:14:54 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const a = 12;
+const b = 5;
+const c = a * b;
+const d = c/2 + (a - b);
+console.log(d);
+
+var user1 = {
+    name: 'Jack',
+    surname: 'Brown',
+    age: 19,
+    gender: 'male',
+};
+
+var user2 = {
+    name: 'Jane',
+    surname: 'Smith',
+    age: 20,
+    gender: 'female',
+};
+
+if (user1.age  >= 18 && user2.age >= 18){
+
+    console.log('group starting from 18');
+} else{
+    console.log('under 18');
+}
+
+console.log(!!!!!!!(a === (b + 7)) && true);
